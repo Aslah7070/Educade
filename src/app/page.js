a3521ae9a1e202637e0c815904f5f7c5aafe00ee
Aslah7070 <aslah.c7070@gmail.com>
2025-05-15 16:23:50 +0530

    completed the next-js and bootstrap set up

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,8 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className="container mt-5">
-      <h1 className="text-primary">Hello, Bootstrap in Next.js 13+</h1>
-      <button className="btn btn-success">Click Me</button>
+      <h1 className="text-primary">Just started</h1>
+      
     </div>
   );
 }
